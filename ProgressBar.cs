12b593f5fa4e2628952413458c8316ebcb42ad88
Brandon Service <brservice@comcast.net>
2018-11-10 14:26:54 -0500

    Add files via upload

---
+++ ProgressBar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ProgressBar : MonoBehaviour {
+
+    public float playerPos { get; set; }
+
+    public Slider progressbar;
+
+	// Use this for initialization
+	void Start () {
+        progressbar.value = CalculatePosition();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.W))
+        {
+            playerPos += 0.01f;
+        }
+	}
+
+    //Calculate player position
+    float CalculatePosition ()
+    {
+        return playerPos;
+    }
+}
